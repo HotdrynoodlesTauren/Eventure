e77350cf7e1ca6eb70c84ddba75b6fa03c1e77af
Yi Zhou <zhou.yi6@northeastern.edu>
2023-11-16 23:44:12 -0800

    Initial Commit

---
+++ Eventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eventure
+//
+//  Created by Yi Zhou on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
